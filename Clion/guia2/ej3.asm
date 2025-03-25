b9b8ea791fea8d39b0f0c320f7c84bfe49fd190b
Valen Ruiz <vruiz@itba.edu.ar>
2025-03-25 11:48:07 -0300

    comienzo ej6

--- Clion/guia2/ej3.asm
+++ Clion/guia2/ej3.asm
@@ -1,21 +1,51 @@
 section .text
+
 GLOBAL _start
-extern toString
-extern print
-extern exit
 
 _start:
-
-    mov eax, 1234
-    mov ebx, reserva
-    call toString
-
+    mov eax, [number]
+    mov ebx, location
+    mov ecx, length
+    call to_string
     call print
-
-    mov ebx, 0
     call exit
+
+to_string:
+    add ebx, ecx
+    dec ebx
+    mov byte [ebx], 10  ; Pongo el salto de linea
+    dec ebx
+    loop:
+        xor edx, edx
+        div ecx
+        add edx, '0'
+        mov byte [ebx], dl
+        dec ebx
+        test eax, eax
+        jnz loop
+
+        ret
+
+print:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, location
+    mov edx, length
+    int 0x80
+
+    ret
+
+exit:
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
+
+
+section .data
+
+number dq 123456789
+length equ 10       ; Se define arbitrariamente 10, pues el lenguaje no permite hacerlo dinámicamente
 
 
 section .bss
-
-reserva resb 10
+location resb length
