837ba2ae1457374ae919cdb718be072c40e0bc78
Valen Ruiz <vruiz@itba.edu.ar>
2025-04-01 12:14:23 -0300

    arreglo ej10

--- Clion/guia2/ej10.asm
+++ Clion/guia2/ej10.asm
@@ -1,40 +1,58 @@
 section .text
-
 GLOBAL _start
 
 _start:
-    mov ecx, [esp]
+    mov edx, [esp]     ; Cargar argc
+    test edx, edx      ; Verificar si argc es 0
     jz exit
 
     mov esi, esp
-    add esi, 4
-    mov ebx, 4
+    add esi, 4         ; Apuntar a argv[0]
 
 toPrintArgs:
-    push ecx
-    mov edi, [esi + ebx]
+    mov ecx, [esi]     ; Obtener dirección del argumento actual
+    test ecx, ecx
+    jz exit            ; Si es NULL, salir
 
-    mov ecx, edi
-    call print
+    call print         ; Llamar a la función de impresión
 
-    pop ecx
-    add ebx, 4
-    dec ecx
-    jnz toPrintArgs
+    add esi, 4         ; Pasar al siguiente argumento
+    dec edx            ; Reducir contador de argumentos
+    jnz toPrintArgs    ; Si quedan argumentos, continuar
 
     call exit
 
 print:
-    mov eax, 4
-    mov ebx, 1
-    mov edx, 50
-    int 80h
+    push edx           ; Guardar edx (lo modificaremos)
+    push ecx           ; Guardar ecx (dirección del string)
 
+    ; Calcular la longitud del string
+    mov edi, ecx       ; Copiar la dirección del string en edi
+    xor eax, eax       ; Limpiar eax
+
+find_length:
+    cmp byte [edi], 0  ; Verificar si es el fin de la cadena
+    jz length_found
+    inc edi
+    inc eax
+    jmp find_length
+
+length_found:
+    mov edx, eax       ; Longitud en edx
+    pop ecx            ; Restaurar ecx (dirección del string)
+
+    ; Llamar a sys_write (imprimir string)
     mov eax, 4
-    mov ecx, endtring
     mov ebx, 1
+    int 0x80
+
+    ; Imprimir un salto de línea
+    mov eax, 4
+    mov ecx, endstring
     mov edx, 1
-    int 80h
+    int 0x80
+
+    pop edx            ; Restaurar edx
     ret
 
 exit:
@@ -43,8 +61,6 @@ exit:
     int 0x80
 
 section .data
+endstring db 10  ; Salto de línea
 
-endtring db 10, 0
-
-section .bss
 
